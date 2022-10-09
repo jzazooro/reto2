8f7ecc4db7c2aa637577cc7f55e5c6373745ce44
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-09 23:54:00 +0200

    commit ruben code

--- funciones/p.py
+++ funciones/p.py
@@ -4,6 +4,7 @@ from tensorflow.keras import layers
 
 
 model = Sequential()
+
 model.add(Dense(5, input_dim=input_dim, activation='soft_plus_te'))
 model.add(Dense(10, activation='soft_plus_te'))
 model.add(Dense(20, activation='tanh'))
@@ -13,7 +14,9 @@ model.add(Dense(20, activation='sigmoid'))
 model.add(Dense(25, activation='relu'))
 model.add(Dense(output_dim, activation='soft_plus_te'))
 model.add(Dropout(0.2))
+
 model.compile(loss='mean_squared_error', optimizer='adam')
+
 model.fit(train_x,train_y,epochs=number_epo,verbose=0,batch_size=10,
 validation_data=(test_x, test_y))
 model.summary()
